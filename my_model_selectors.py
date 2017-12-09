57fff94e5be244db10954d4a41799117daf8f78d
nbeshouri <nbeshouri@gmail.com>
2017-12-08 22:39:32 -0500

    Add solutions

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -1,19 +1,19 @@
 import math
-import statistics
 import warnings
-
 import numpy as np
 from hmmlearn.hmm import GaussianHMM
 from sklearn.model_selection import KFold
 from asl_utils import combine_sequences
+from typing import Dict, Tuple, List, Callable, Union
 
 
 class ModelSelector(object):
-    '''
-    base class for model selection (strategy design pattern)
-    '''
+    """
+    Base class for model selection (strategy design pattern).
 
-    def __init__(self, all_word_sequences: dict, all_word_Xlengths: dict, this_word: str,
+    """
+
+    def __init__(self, all_word_sequences: Dict[str, List], all_word_Xlengths: Dict[str, Tuple], this_word: str,
                  n_constant=3,
                  min_n_components=2, max_n_components=10,
                  random_state=14, verbose=False):
@@ -28,13 +28,11 @@ class ModelSelector(object):
         self.random_state = random_state
         self.verbose = verbose
 
-    def select(self):
+    def select(self) -> GaussianHMM:
         raise NotImplementedError
 
-    def base_model(self, num_states):
-        # with warnings.catch_warnings():
+    def base_model(self, num_states) -> Union[GaussianHMM, None]:
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-        # warnings.filterwarnings("ignore", category=RuntimeWarning)
         try:
             hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
                                     random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
@@ -46,63 +44,141 @@ class ModelSelector(object):
                 print("failure on {} with {} states".format(self.this_word, num_states))
             return None
 
+    def find_best_model(self, score_func: Callable[[int], float], selection_func: Callable) -> GaussianHMM:
+        """
+        Return the best `GaussianHMM` for the word.
+
+        This is just a helper method to prevent having to duplicate the
+        decision logic in each `ModelSelector` subclass.
+
+        Args:
+            score_func: A function to score models. It should take the number
+                of state as an int and return the score as float or return
+                `None` if a score can't be computed due to a model error.
+            selection_func: A decision function to choose between models,
+                either `max` or `min`.
+
+        Returns:
+            The best `GaussianHMM` for the word.
+
+        """
+        scores = ((score_func(i), i) for i in range(self.min_n_components, self.max_n_components + 1))
+        scores = [score for score in scores if score[0]]
+        if scores:
+            # If if the score function was able to score at least one
+            # model, best one.
+            _, best_n = selection_func(scores, key=lambda score: score[0])
+            return self.base_model(best_n)
+        else:
+            # If all else fails, return a 3 state model.
+            return self.base_model(3)
+
 
 class SelectorConstant(ModelSelector):
-    """ select the model with value self.n_constant
+    """
+    Select the model with value self.n_constant.
 
     """
 
-    def select(self):
-        """ select based on n_constant value
+    def select(self) -> GaussianHMM:
+        """
+        Select based on n_constant value.
 
-        :return: GaussianHMM object
         """
         best_num_components = self.n_constant
         return self.base_model(best_num_components)
 
 
 class SelectorBIC(ModelSelector):
-    """ select the model with the lowest Bayesian Information Criterion(BIC) score
+    """
+    Select the model with the lowest Bayesian Information Criterion(BIC) score.
 
-    http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
+    See: http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
+
     Bayesian information criteria: BIC = -2 * logL + p * logN
+
     """
 
-    def select(self):
-        """ select the best model for self.this_word based on
-        BIC score for n between self.min_n_components and self.max_n_components
+    def select(self) -> GaussianHMM:
+        """
+        Select the best model for self.this_word based on BIC score for
+        n between self.min_n_components and self.max_n_components.
 
-        :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
         # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+
+        def bic_score(num_states: int) -> Union[float, None]:
+            try:
+                model = self.base_model(num_states)
+                log_l = model.score(self.X, self.lengths)
+            except:
+                return None
+            num_features = model.n_features
+            num_params = num_states * (num_states - 1) + (num_states - 1) + 2 * num_features * num_states
+            num_data_points = len(self.X)
+            return (-2. * log_l) + (num_params * math.log(num_data_points))
+
+        return self.find_best_model(bic_score, min)
 
 
 class SelectorDIC(ModelSelector):
-    ''' select best model based on Discriminative Information Criterion
+    """
+    Select best model based on Discriminative Information Criterion.
 
-    Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
+    See: Biem, Alain. "A model selection criterion for classification: Application to hmm topology optimization."
     Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
+
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
-    '''
 
-    def select(self):
+    """
+
+    def select(self) -> GaussianHMM:
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
         # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+
+        def dic_score(num_states: int) -> Union[float, None]:
+            try:
+                model = self.base_model(num_states)
+                log_l = model.score(self.X, self.lengths)
+                total_anti_log_l = sum(model.score(*X_lengths) for word, X_lengths in self.hwords.items() if word != self.this_word)
+            except:
+                return None
+            return log_l - (total_anti_log_l / (len(self.words) - 1))
+
+        return self.find_best_model(dic_score, max)
 
 
 class SelectorCV(ModelSelector):
-    ''' select best model based on average log Likelihood of cross-validation folds
+    """
+    Select best model based on average log Likelihood of cross-validation folds.
 
-    '''
+    """
 
-    def select(self):
+    def select(self) -> GaussianHMM:
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
         # TODO implement model selection using CV
-        raise NotImplementedError
+
+        def cv_score(num_states: int) -> Union[float, None]:
+            if len(self.sequences) > 1:
+                split_method = KFold(n_splits=min(len(self.sequences), 3))
+            else:
+                return None
+
+            scores = []
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                train_X, train_lengths = combine_sequences(cv_train_idx, self.sequences)
+                test_X, test_lengths = combine_sequences(cv_test_idx, self.sequences)
+                model = GaussianHMM(n_components=num_states,
+                                    covariance_type="diag", n_iter=1000,
+                                    random_state=self.random_state,
+                                    verbose=False)
+                try:
+                    model.fit(train_X, train_lengths)
+                    scores.append(model.score(test_X, test_lengths))
+                except:
+                    return None
+            return np.mean(scores)
+
+        return self.find_best_model(cv_score, max)
--- my_recognizer.py
+++ my_recognizer.py
@@ -1,25 +1,169 @@
 import warnings
 from asl_data import SinglesData
+import math
+import io
+import os
+import re
+import pandas as pd
+from collections import deque
+from functools import lru_cache
+from hmmlearn.hmm import GaussianHMM
+from typing import Dict, Tuple, List
 
 
-def recognize(models: dict, test_set: SinglesData):
-    """ Recognize test word sequences from word models set
+lm_dataframe = None
 
-   :param models: dict of trained models
-       {'SOMEWORD': GaussianHMM model object, 'SOMEOTHERWORD': GaussianHMM model object, ...}
-   :param test_set: SinglesData object
-   :return: (list, list)  as probabilities, guesses
-       both lists are ordered by the test set word_id
-       probabilities is a list of dictionaries where each key a word and value is Log Liklihood
-           [{SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
-            {SOMEWORD': LogLvalue, 'SOMEOTHERWORD' LogLvalue, ... },
-            ]
-       guesses is a list of the best guess words ordered by the test set word_id
-           ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
-   """
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
-    probabilities = []
-    guesses = []
+
+def recognize(models: Dict[str, GaussianHMM], test_set: SinglesData, lm_weight=0, beam_size=1) -> Tuple[List, List]:
+    """
+    Recognize test word sequences from word models set
+
+    Args:
+        models: `dict` of trained models.
+        test_set: `SinglesData` representing the test set.
+        lm_weight: The multiplier applied to to the language model's log
+            likelihood estimate for a word before it's added to the HMM's
+            estimate. The default value of 0 effectively disables the
+            language model.
+        beam_size: The number of alternative guesses retained for later
+            consideration as the recognizer guesses each word. This only
+            affects the outcome if the language model is used. The default
+            value of 1 is equivalent to a greedy, best-first search.
+
+    Returns:
+        (list, list)  as probabilities, guesses
+
+    """
     # TODO implement the recognizer
-    # return probabilities, guesses
-    raise NotImplementedError
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    guesses = []
+    probabilities = []
+
+    def get_word_log_l(model: GaussianHMM, seq_index: int, word: str, prev_words: Tuple[str]) -> float:
+        """Return word log likelihood given data, HMM, and prev words."""
+        lm_log_l = get_lm_log_l(word, tuple(prev_words[-2:])) if lm_weight else 0.0
+        hmm_log_l = get_model_log_l(model, test_set, seq_index)
+        return (lm_weight * lm_log_l) + hmm_log_l
+
+    # Iterate over the sentences in the test set, which are represented
+    # as list of sequence indexes. These indexes can be used to get
+    # (X, lengths) from test_set.
+    for sentence in test_set.sentences_index.values():
+
+        def get_successors(path: Tuple[str]) -> List[Tuple[str]]:
+            """Return beam_size most likely successor paths."""
+            # Calulate word probs for the next word in the sentence.
+            seq_index = sentence[len(path)]
+            word_probs = {word: get_word_log_l(model, seq_index, word, path)
+                          for word, model in models.items()}
+            # Convert them into a sorted sequence of (word, word_prob) pairs.
+            word_prob_pairs = sorted(word_probs.items(), key=lambda x: x[1])
+            # Return successor paths for the beam_size most likely next words.
+            return [path + (pair[0],) for pair in word_prob_pairs[-beam_size:]]
+
+        # Conduct a search through space of possible sentences. If beam_size
+        # is 1, this is equivalent to greedily picking the highest scoring
+        # word.
+        frontier = deque(get_successors(tuple()))
+        candidates = []
+        while frontier:
+            path = frontier.popleft()
+            if len(path) == len(sentence):
+                candidates.append(path)
+            else:
+                for successor in get_successors(path):
+                    frontier.append(successor)
+
+        def get_candidate_log_l(candidate: Tuple[str]) -> float:
+            """Return log likelihood of a candidate sentence."""
+            return sum([get_word_log_l(models[word], sentence[i], word, candidate[:i])
+                        for i, word in enumerate(candidate)])
+
+        # Pick the best candidate sentence.
+        sentence_guess = max(candidates, key=lambda c: get_candidate_log_l(c))
+        # Extend the cumulative guesses and probabilities list.
+        guesses.extend(sentence_guess)
+        probabilities.extend([{word: get_word_log_l(model, seq_index, word, sentence_guess[:i])
+                               for word, model in models.items()}
+                               for i, seq_index in enumerate(sentence)])
+
+    return probabilities, guesses
+
+
+@lru_cache(maxsize=None)
+def get_model_log_l(model: GaussianHMM, test_set: SinglesData, sequence_index: int) -> float:
+    """Return log(P(X|model) according to the HMM."""
+    try:
+        X, lengths = test_set.get_item_Xlengths(sequence_index)
+        return model.score(X, lengths)  # Log(P(X|model)
+    except:
+        return -math.inf  # Log(0)
+
+
+@lru_cache(maxsize=None)
+def get_lm_log_l(word: str, prev_words: Tuple[str]) -> float:
+    """Return log(P(word|prev_words)) according to the language model."""
+
+    # Load the language model Dataframe if it isn't loaded already.
+    global lm_dataframe
+    if lm_dataframe is None:
+        lm_dataframe = get_lm_dataframe()
+
+    # If there are no prev words or only one, append '<s>' tag used in the file
+    # to indicate the start of a sentence.
+    if not prev_words:
+        prev_words = ('', '<s>')
+    elif len(prev_words) < 2:
+        prev_words = ('<s>',) + prev_words
+    else:
+        prev_words = prev_words[-2:]
+
+    # Convert the word str and prev_word tuple into trigram, bigram, and
+    # unigram strings.
+    n_grams = [' '.join(prev_words + (word,)), ' '.join(prev_words[1:] + (word,)), word]
+
+    # Some signs have multiple versions (e.g. 'GO1', 'GO1'). Use regex
+    # to strip off the trailing digits from words in the n-grams.
+    n_grams = [re.sub(r'(?<=\w)\d(?=\s|$)', '', n_gram) for n_gram in n_grams]
+
+    # Try to find the n-grams, starting with the trigram, in the language
+    # model.  Simpler n-grams will have a non-zero back-off weight added to
+    # them to so that can be used in the trigram model.
+    for n_gram in n_grams:
+        if n_gram in lm_dataframe.index:
+            likelihood = lm_dataframe.loc[n_gram]['log_likelihood']
+            backoff_weight = lm_dataframe.loc[n_gram]['backoff_weight']
+            # The ARPA format specifies base 10 log likelihoods, but
+            # hmmlearn appears to return base e log likelihoods, so
+            # change the base before returning.
+            return (likelihood + backoff_weight) / math.log(math.e, 10)
+
+    # All words should be in the lm file, at least as unigrams, so
+    # if we can't find a match something has gone wrong.
+    raise Exception(f'"{word}" was not found in the language model.')
+
+
+def get_lm_dataframe() -> pd.DataFrame:
+    """Return a `DataFrame` that represents the language model."""
+    # Look for the language model file.
+    path = os.path.join(os.getcwd(), 'data', 'ukn.3.lm')
+    if not os.path.isfile(path):
+        path = os.path.join(os.getcwd(), 'ukn.3.lm')
+    if not os.path.isfile(path):
+        raise FileNotFoundError('The "ukn.3.lm" language model file was '
+                                'not found in the data directory.')
+    with open(path, 'r') as f:
+        lm_str = f.read()
+    # Strip out everything but the lines containing n-gram data.
+    lines = re.findall(r'^-\d.*\n', lm_str, re.MULTILINE)
+    lm_str = ''.join(lines)
+    # Add column names to the top of the data lines.
+    lm_str = 'log_likelihood\tn_gram\tbackoff_weight\n' + lm_str
+    # Parse the str into a DataFrame.
+    lm_dataframe = pd.read_csv(io.StringIO(lm_str), sep='\t')
+    # Replace the index with the n-gram strings.
+    lm_dataframe.set_index('n_gram', inplace=True)
+    lm_dataframe.sort_index()
+    # Replace NaN back-off weights with zeroes.
+    lm_dataframe.fillna(0.0, inplace=True)
+    return lm_dataframe
